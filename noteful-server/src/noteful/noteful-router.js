236dd8b0bc72a1c35075a853caa59c9f1bb42d8e
andrewbituin <fandrewbituin@gmail.com>
2019-03-01 14:34:00 -0800

    working on delete functionality

--- noteful-server/src/noteful/noteful-router.js
+++ noteful-server/src/noteful/noteful-router.js
@@ -23,8 +23,8 @@ const serializeNotes = note => {
     content: xss(note.content),
     modified: xss(note.modified),
     folderId: note.folders_id
-  }
-}
+  };
+};
 notefulRouter
   .route("/folders")
   .get((req, res, next) => {
@@ -47,46 +47,110 @@ notefulRouter
     if (typeof name !== "string") {
       return res.status(400).send("Name must be type of string.");
     }
-    NotefulService.insertFolders(knexInstance, newFolder).then(folder => {
-      logger.info(`Folder named ${folder.folders_name} was created.`)
-      res
-        .status(201)
-        .location(path.posix.join(req.originalUrl, `/${folder.id}`))
-        .json(serializeFolders(folder));
-    })
-    .catch(next)
+    NotefulService.insertFolders(knexInstance, newFolder)
+      .then(folder => {
+        logger.info(`Folder named ${folder.folders_name} was created.`);
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${folder.id}`))
+          .json(serializeFolders(folder));
+      })
+      .catch(next);
   });
 
 notefulRouter
   .route("/folders/:folderId")
-  .get((req, res) => {
-    
-  })
-  .post(bodyParser, (req, res) => {
-    // POST to noteful
+  .delete(bodyParser, (req, res, next) => {
+    const knexInstance = req.app.get("db");
+    const { folderId } = req.params;
+    NotefulService.getFolderById(knexInstance, folderId).then(folder => {
+      if (!folder) {
+        logger.info(`Folder not found with id ${folderId}.`);
+        return res.status(404).json({
+          error: {
+            message: "Folder not found."
+          }
+        });
+      }
+    });
+    NotefulService.deleteFolder(knexInstance, folderId)
+      .then(numRowsAffected => {
+        logger.info(`Folder with id ${folderId} deleted.`);
+        res.status(204).end();
+      })
+      .catch(next);
   });
 
 notefulRouter
   .route("/notes")
-    .get((req, res, next) => {
-      const knexInstance = req.app.get("db");
-      NotefulService.getAllNotes(knexInstance)
-        .then(notes => {
-          res.json(notes.map(serializeNotes));
-        })
-        .catch(next);
+  .get((req, res, next) => {
+    const knexInstance = req.app.get("db");
+    NotefulService.getAllNotes(knexInstance)
+      .then(notes => {
+        res.json(notes.map(serializeNotes));
+      })
+      .catch(next);
   })
-    .post((req, res, next) => {
-    
+  .post(bodyParser, (req, res, next) => {
+    const knexInstance = req.app.get("db");
+    const { name, content, folderId } = req.body;
+    const newNote = {
+      notes_name: name,
+      content,
+      folders_id: folderId
+    };
+    for (const field of ["notes_name", "content", "folders_id"]) {
+      if (!newNote[field]) {
+        logger.info(`${field} is required.`);
+        return res.status(400).send(`${field} is required.`);
+      }
+    }
+    if (typeof name !== "string") {
+      logger.info(`Name: ${name} must be typeof string.`);
+      return res.status(400).send(`Name: ${name} must be typeof string.`);
+    }
+    if (typeof content !== "string") {
+      logger.info(`Content: ${content} must be typeof string.`);
+      return res.status(400).send(`Content: ${content} must be typeof string.`);
+    }
+    if (typeof folderId !== "string") {
+      logger.info(`Folder ID: ${folderId} must be typeof string.`);
+      return res
+        .status(400)
+        .send(`Folder ID: ${folderId} must be typeof string.`);
+    }
+    NotefulService.insertNotes(knexInstance, newNote)
+      .then(note => {
+        logger.info(`Note named ${note.notes_name} was created.`);
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${note.id}`))
+          .json(serializeNotes(note));
+      })
+      .catch(next);
   });
 
-notefulRouter
-  .route("/notes/:notesId")
-  .get((req, res) => {
-    // GET notefuls.notefulId
+notefulRouter.route("/notes/:noteId").delete((req, res, next) => {
+  const knexInstance = req.app.get("db");
+  const { noteId } = req.params;
+  NotefulService.getNoteById(knexInstance, noteId).then(note => {
+    if (!note) {
+      logger.info(`Note not found with id ${noteId}.`);
+      return res.status(404).json({
+        error: {
+          message: "Note not found."
+        }
+      });
+    }
+    next();
   })
-  .delete((req, res) => {
-    // DELETE notefuls.notefulId
-  });
+    .catch(next);
+  NotefulService.deleteNote(knexInstance, noteId)
+  .then(numRowsAffected => {
+    logger.info(`Note with id ${noteId} deleted.`);
+    res.status(204).end();
+  })
+  .catch(next);
+});
 
 module.exports = notefulRouter;
--- noteful-server/src/noteful/noteful-service.js
+++ noteful-server/src/noteful/noteful-service.js
@@ -3,7 +3,7 @@ const NotefulService = {
     return knex('folders').select('*');
   },
 
-  getById(knex) {
+  getFolderById(knex, id) {
     return knex('folders')
       .select('*')
       .where('id', id)
@@ -17,8 +17,32 @@ const NotefulService = {
         return rows[0];
       });
   },
+  deleteFolder(knex, id){
+    return knex('folders')
+      .where({ id })
+      .delete()
+  },
   getAllNotes(knex) {
     return knex('notes').select('*');
+  },
+  getNoteById(knex, id) {
+    return knex('notes')
+      .select('*')
+      .where('id', id)
+      .first();
+  },
+  insertNotes(knex, newNote){
+    return knex('notes')
+      .insert(newNote)
+      .returning('*')
+      .then(rows =>{
+        return rows[0];
+      });
+  },
+  deleteNote(knex, id){
+    return knex('notes')
+      .where({ id })
+      .delete()
   }
 };
 
